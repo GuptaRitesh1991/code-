9690598fe5551d4f2b759355a132cdad22333211
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-10-23 13:38:57 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14445
+let accountEmail = "Abc@gmail.com"
+var accountPassword = "12345678"
+accountState = "hbj"
+let accountcity
+accountEmail = "cbz@gmail.com"
+accountPassword = "Abc@1234"
+accountState = 
+
+console.table([accountId, accountEmail, accountPassword, accountState, accountcity]);
+//console.log(accountId);
+/* Prefer not to use Var, becasue of issue of Block scope
+and Functional scope */
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code as new version
+
+
+// Type of Data Types (premetive)
+// number => 2 to the power 53
+// bigint => used mainly for share market application to deal big numbers
+// string => ""
+// boolean => True/False
+// null => Standalone Value
+// undefined 
+// symbol => unique
+
+
+// alert(3+3) // we are using node.js not browser
+console.log(typeof null); // will show object
+console.log(typeof undefined); // will show undefined
+
