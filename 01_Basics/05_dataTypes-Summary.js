a5995261989c77d8af75b1ef90886b0c2573b24f
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-10-26 14:49:46 +0000

    save

--- 01_Basics/05_dataTypes-Summary.js
+++ 01_Basics/05_dataTypes-Summary.js
@@ -17,6 +17,28 @@ const anotherId = Symbol ("123")
 
 const heros = ["shakti","Pandey","fatichar"]
 
-// anything under {} are objects
+// anything under {} are objects, when you check typeof u will get object, 
+// when you check function typeof you will get object function 
 
 
+// +++++++++++++++++++++++++++++++++++++++++ Memory =======================
+// Stack(all premetive type)creates a copy and Heap (non premetive) gives a reference
+
+let myYoutubeName = "Ritzygupta"
+let anotherName = myYoutubeName
+anotherName = "Chaiwala"
+
+//console.log(myYoutubeName);
+//console.log(anotherName);
+
+let user1 = {
+
+    email:"user@google.com" ,
+    upiId: '89898@sbi'
+}
+
+let user2 = user1
+
+user2.email = "hitesh@google.com"
+console.log(user1.email);
+console.log(user2.email);
