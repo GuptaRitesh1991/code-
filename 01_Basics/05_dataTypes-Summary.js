242a98797a7249f11f385588aacdcd9b4dad9617
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-10-26 13:38:09 +0000

    data type summary

---
+++ 01_Basics/05_dataTypes-Summary.js
@@ -0,0 +1,22 @@
+// #Premetive
+// 7 types: string, number, null, boolean, undefined, symbol, bigInt
+
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail
+const id = Symbol('123')
+const anotherId = Symbol ("123")
+
+//console.log(id === anotherId)
+//console.log(typeof anotherId);
+
+
+// ========= Non-Premetive or reference type ==============
+// array, objects, functions
+
+const heros = ["shakti","Pandey","fatichar"]
+
+// anything under {} are objects
+
+
