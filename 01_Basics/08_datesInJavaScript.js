70e366e0b48a09e5bb8408c22e0a5ccb0654457e
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-10-31 15:30:05 +0000

    dates in JS

---
+++ 01_Basics/08_datesInJavaScript.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleDateString());
+
+let myCreatedDate = new Date(2023,0,24)
+console.log(myCreatedDate.toDateString());
