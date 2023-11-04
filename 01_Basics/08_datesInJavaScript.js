6a64a0313c450ac7affb705abdfca33f15b2c602
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-11-04 09:47:12 +0000

    date in Java

--- 01_Basics/08_datesInJavaScript.js
+++ 01_Basics/08_datesInJavaScript.js
@@ -5,5 +5,13 @@ let myDate = new Date()
 //console.log(myDate.toLocaleString());
 //console.log(myDate.toLocaleDateString());
 
-let myCreatedDate = new Date(2023,0,24)
-console.log(myCreatedDate.toDateString());
+//let myCreatedDate = new Date(2023,0,24)
+//console.log(myCreatedDate.toDateString());
+
+//let myCreatedDate = new Date(2023,0,24,5,5)
+//console.log(myCreatedDate.toLocaleString());
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(Math.floor(Date.now()/1000));// Gives values in second date.now/1000
+
+
