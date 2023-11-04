cd080f5b6d0b7b15d426da080b70ef043929c639
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-11-04 15:27:09 +0000

    object

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,15 @@
+// two ways to declare obects, 1 by literals and another by constructor 
+//Singleton = object created from constructor - no multiple insantance  (object.create)method is used.
+
+
+
+// Object literals
+const jsUser = {
+
+    name: "Ritesh"
+    age: 27
+    place: "mumbai"
+    email:"abc@gmail.com"
+    isLoggedIn: false
+    lastLoginDays:["Monday","Saturday"]
+                }
