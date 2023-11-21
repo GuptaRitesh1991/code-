b7e757c432312f2fd12f655a2953391c441104e0
GuptaRitesh1991 <119869728+GuptaRitesh1991@users.noreply.github.com>
2023-11-21 14:03:30 +0000

    While loop

---
+++ 05_itterations/two.js
@@ -0,0 +1,34 @@
+// While loop
+// while (condition) {
+    
+// }
+// let index = 0
+// while (index <= 10) {
+
+//     console.log(`value of index ${index}`);
+//     index = index+2
+    
+// }
+
+const myArr = ["A","B","RRR"]
+let arr = 0
+while (arr < myArr.length) {
+
+    console.log(myArr[arr]);
+  arr = arr + 1
+}
+
+
+// do while loop , pahle kaam hoga fir conition check hoga 
+
+// do {
+    
+// } while (condition);
+
+score = 1
+
+do {
+
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
